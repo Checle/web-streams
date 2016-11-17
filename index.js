f3c2a41286fdcfc3b9a47754529042239e679960
Filip Dalüge <daluege@gmail.com>
2016-11-17 23:45:26 +0100

    Add ES5 build

---
+++ index.js
@@ -0,0 +1,4 @@
+Object.assign(exports, require('./lib/readable-stream'));
+Object.assign(exports, require('./lib/writable-stream'));
+Object.assign(exports, require('./lib/transform-stream'));
+Object.assign(global, exports);
---
+++ webpack.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015']
+        }
+      }
+    ]
+  }
+}
